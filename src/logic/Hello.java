d0b524ca22d35c1224c44083403c252ef38616ba
Puttiphat <Puttiphat@10.207.179.217>
2016-01-21 16:51:15 +0700

    eieiei

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -3,31 +3,44 @@ package logic;
 public class Hello {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(Hello.greetingMessage());
-		
-		int[] array = {5,4,3,2,1};
-		
+
+		int[] array = { 5, 4, 3, 2, 1 };
+
 		System.out.println(Hello.arrayToString("Array", array));
 		array = Hello.bubbleSort(array);
 		System.out.println(Hello.arrayToString("Sorted Array", array));
 	}
 
-	public static String greetingMessage(){
+	public static String greetingMessage() {
 		// TODO Implement this method to return string "Hello GitHub".
-		return "fifamd";
+		return "Hello GitHub";
 	}
-	
-	public static int[] bubbleSort(int[] array){
-		// TODO Implement this method to sort the given array in an ascending order and return the sorted array.
+
+	public static int[] bubbleSort(int[] array) {
+		// TODO Implement this method to sort the given array in an ascending
+		// order and return the sorted array.
+		boolean swap = true;
+		while (swap) {
+			swap = false;
+			for (int i = 0; i < array.length-1; i++) {
+				if (array[i] >= array[i + 1]) {
+					int temp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
+					swap = true;
+				}
+			}
+		}
+
 		return array;
 	}
-	
-	public static String arrayToString(String title,int[] array){
-		
+
+	public static String arrayToString(String title, int[] array) {
+
 		String str = title + " : ";
-		for (int i = 0;i < array.length;i++)
-		{
+		for (int i = 0; i < array.length; i++) {
 			str += array[i] + " ";
 		}
 		return str;
